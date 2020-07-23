f992121ea08e80b6f13a8e617869138d7491f2b1
emysly <emysilva@users-MacBook-Pro.local>
2020-07-23 23:05:20 +0400

    barking dog implementation

--- src/com/emysilva/Main.java
+++ src/com/emysilva/Main.java
@@ -3,6 +3,14 @@ package com.emysilva;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(shouldWakeUp(true, 21));
+    }
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (hourOfDay < 8 || hourOfDay > 22) {
+            barking = true;
+            return barking;
+        }
+        barking = false;
+        return barking;
     }
 }
